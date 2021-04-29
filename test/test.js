ef52dcf8e205b4bc68245d07eede900a19952c50
Roman Meredith <10769789@uvu.edu>
2021-04-28 20:45:39 -0600

    buildspec.yml

--- test/test.js
+++ test/test.js
@@ -17,6 +17,7 @@ describe('Tests', function() {
       chai.request(server)
       .get('/')
       .end((err, res) => {
+        console.log(res)
         res.should.have.status(200)
         done()
       })
