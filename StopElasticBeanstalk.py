fd8b94f8e11e93ff5e96817b4350d93eafd5f255
Roman Meredith <10769789@uvu.edu>
2021-11-03 19:48:53 -0600

    start and stopping elastic beanstalk instances

---
+++ StopElasticBeanstalk.py
@@ -0,0 +1,14 @@
+import boto3
+envid=['e-cacfmyngzp']
+client = boto3.client('elasticbeanstalk')
+def handler(event, context):
+    try:
+         for appid in range(len(envid)):
+             response = client.terminate_environment(EnvironmentId=str(envid[appid].strip()))
+             if response:
+                 print('Terminating environment %s' %str(envid[appid]))
+             else:
+                 print('Failed to Terminate environment %s' %str(envid[appid]))
+             
+    except Exception as e:
+        print(e)
